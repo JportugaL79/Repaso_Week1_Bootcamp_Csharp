de0d7f42f55206484461c96fb18dc79feb220db4
Jesús Portugal <jeshus.portugal@gmail.com>
2025-04-06 23:29:08 -0500

    Cambios de cálcular 3 números, y calcular áreas

--- Repaso_Week1_Bootcamp_Csharp/Program.cs
+++ Repaso_Week1_Bootcamp_Csharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -44,7 +45,7 @@ namespace Repaso_Week1_Bootcamp_Csharp
             Console.WriteLine("====================");
 
             //Comprobar si un numero es PAR o IMPAR
-            int numComprobar;
+            /*int numComprobar;
             Console.WriteLine("===============================");
             Console.WriteLine("Ingresa el valor a comprobar");
             Console.WriteLine("===============================");
@@ -57,26 +58,79 @@ namespace Repaso_Week1_Bootcamp_Csharp
             else
             {
                 Console.WriteLine("El numero es IMPAR");
-            }
+            }*/
 
-            //Comprobar si un numero es PAR o IMPAR
+            //Cuál es el número mayor de 3 números
             int primerNumero;
             int segundoNumero;
             int tercerNumero;
-            int numMayor;
             Console.WriteLine("");
             Console.WriteLine("===============================");
             Console.WriteLine("Cuál es el número mayor de 3 números");
             Console.WriteLine("===============================");
-            numMayor= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Escribe el 1er número");
+            primerNumero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe el 2do número");
             segundoNumero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe el 3er número");
             tercerNumero = Convert.ToInt32(Console.ReadLine());
+            if (primerNumero > segundoNumero) 
+            {
+                if (primerNumero > tercerNumero)
+                {
+                    Console.WriteLine("El número mayor es el 1er ");
+                }
+                else 
+                {
+                    Console.WriteLine("El número mayor es el 3er ");
+                }
+            }
+            else if (segundoNumero > tercerNumero) 
+            {
+                Console.WriteLine("El número mayor es el 2do ");
+            }
+            else 
+            {
+                Console.WriteLine("El número mayor es el 3er ");
+            }
 
-            // Escribir la lógica del programa
-
+                // Obtener área del Circulo y Rectangulo
+                String primerArea;
+            double Pi = 3.14;
+            double radioCirculo;
+            double areaCirculo;
+            double basee;
+            double altura;
+            double areaRectangulo;
+            Console.WriteLine("");
+            Console.WriteLine("===============================");
+            Console.WriteLine("Calculando Areas");
+            Console.WriteLine("===============================");
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese el área a trabajar");
+            primerArea = Console.ReadLine();
+            //segundoArea = Convert.ToInt32(Console.ReadLine());
+            //tercerNumero = Convert.ToInt32(Console.ReadLine());
+            switch (primerArea)
+            {
+                case "1": //Círculo
+                    Console.WriteLine("Ingrese rádio del Círculo");
+                    radioCirculo = Convert.ToInt32(Console.ReadLine());
+                    areaCirculo = radioCirculo * Pi;
+                    Console.WriteLine("El área del Círculo es: " + areaCirculo);
+                    break;
+                default: //"Réctangulo":
+                    Console.WriteLine("Ingrese Base del Réctangulo");
+                    basee = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingrese Altura del Réctangulo");
+                    altura = Convert.ToInt32(Console.ReadLine());
+                    areaRectangulo = basee * altura;
+                    Console.WriteLine("El área del Réctangulo es: " + areaRectangulo);
+                    break;
+            }
 
         }
-
     }
 }
  
